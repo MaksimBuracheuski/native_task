43e77c01d4735d63bdeeebaf5f4093f8220a8bab
“MBuracheuski” <mburacheuski@solvd.com>
2024-03-14 17:24:23 +0300

    Implemented API tests using Carina

---
+++ src/main/java/com/gorest/GetUserByIdMethod.java
@@ -0,0 +1,16 @@
+package com.gorest;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+
+
+@Endpoint(url = "https://gorest.co.in/public/v2/users/${id}", methodType = HttpMethodType.GET)
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+public class GetUserByIdMethod extends AbstractApiMethodV2 {
+    public GetUserByIdMethod(String id) {
+        replaceUrlPlaceholder("id", id);
+    }
+}
